efe39166603a0a24f55d4116fc0e0391298a37fe
Mashal Afzal <mashal.afzal2996@gmail.com>
2024-05-21 20:44:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+//console.log(number)
+//let number=50
+//console.log(number2)
+//const number=10 
+let number = 50;
+console.log(number);
---
+++ app.ts
@@ -0,0 +1,9 @@
+//console.log(number)
+//let number=50
+
+//console.log(number2)
+    //const number=10 
+
+    let number=50
+    console.log(number)
+
